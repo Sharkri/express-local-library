df528081530889dcbbc15dc34d27160fa44355c8
Sharkri <sharkdude955@gmail.com>
2023-04-22 22:42:50 -0500

    Add book instance delete functionality

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -93,12 +93,26 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    const err = new Error("Book instance not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete book instance",
+    book_instance: bookInstance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  await BookInstance.findByIdAndRemove(req.body.bookinstance_id).exec();
+  res.redirect("/catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
